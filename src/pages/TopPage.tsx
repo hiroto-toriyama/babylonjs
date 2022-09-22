1106141b5db51daf03e24556475c1d55a171b6f8
h.toriyama <toriyamahiroto1128@gmail.com>
2022-09-23 00:24:16 +0900

    min-h-screen

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -4,7 +4,7 @@ import Village from "../components/Village";
 const TopPage = () => {
   return (
     <div className="bg-base-200">
-      <div className="container mx-auto p-5 lg:max-w-4xl bg-base-100">
+      <div className="container mx-auto p-5 lg:max-w-4xl bg-base-100 min-h-screen">
         <div className="font-bold text-5xl mb-5 border-b-8">Babyron.js</div>
         <div className="flex flex-col gap-5">
           <Box />
